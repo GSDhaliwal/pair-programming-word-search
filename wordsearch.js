4dff1b525a46c65e225b5d443d57f75a217a9c48
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-02 02:25:27 +0000

    added stretch goal to search for word in reverse

--- wordsearch.js
+++ wordsearch.js
@@ -1,7 +1,11 @@
-const wordSearch = (letters, word) => { 
-  const horizontalJoin = letters.map(ls => ls.join(''))
-  for (l of horizontalJoin) {
-      if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  const reverseHJ = letters.map(ls => ls.reverse().join(''));
+  for (let l of reverseHJ) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
   }
   let result = [];
   for (let row = 0; row < letters.length; row++) {
@@ -10,11 +14,18 @@ const wordSearch = (letters, word) => {
       result[row][col] = letters[col][row];
     }
   }
-  const verticalJoin = result.map(ls => ls.join(''))
-  for (l of verticalJoin) {
-      if (l.includes(word)) return true
-  }
-  return false;  
-}
 
-module.exports = wordSearch
+  const verticalJoin = result.map(ls => ls.join(''));
+  const reverseVJ = result.map(ls => ls.reverse().join(''));
+  for (let l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+  for (let l of reverseVJ) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+
+module.exports = wordSearch;
