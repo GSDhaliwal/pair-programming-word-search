c1683a21f991a275525b3b9706a7b9d25e624ae5
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-02 01:12:19 +0000

    paired programming corrected

--- wordsearch.js
+++ wordsearch.js
@@ -4,18 +4,17 @@ const wordSearch = (letters, word) => {
       if (l.includes(word)) return true
   }
   let result = [];
-  for (let row = 0; row < row.length; row++) {
+  for (let row = 0; row < letters.length; row++) {
     result[row] = [];
     for (let col = 0; col < letters.length; col++) {
       result[row][col] = letters[col][row];
     }
   }
-  const verticalJoin = letters.map(ls => ls.join(''))
+  const verticalJoin = result.map(ls => ls.join(''))
   for (l of verticalJoin) {
       if (l.includes(word)) return true
   }
   return false;  
 }
-
 
 module.exports = wordSearch
