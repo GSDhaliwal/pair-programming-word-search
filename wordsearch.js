2c06707aca595257cbd59fedd702250b337892ba
Gurcharan S Dhaliwal <gurcharan.dhaliwal86@gmail.com>
2020-07-02 00:56:55 +0000

    paired programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,21 @@
 const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+  const horizontalJoin = letters.map(ls => ls.join(''))
+  for (l of horizontalJoin) {
+      if (l.includes(word)) return true
+  }
+  let result = [];
+  for (let row = 0; row < row.length; row++) {
+    result[row] = [];
+    for (let col = 0; col < letters.length; col++) {
+      result[row][col] = letters[col][row];
     }
+  }
+  const verticalJoin = letters.map(ls => ls.join(''))
+  for (l of verticalJoin) {
+      if (l.includes(word)) return true
+  }
+  return false;  
 }
+
 
 module.exports = wordSearch
